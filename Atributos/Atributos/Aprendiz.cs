e8356481ae5ceba4e1073d46867ee6e797001c22
cristian_jimenez <crisjimenez0303@gmail.com>
2020-03-02 11:46:23 -0500

    guardar archivos recientes

---
+++ Atributos/Atributos/Aprendiz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Atributos
+{
+    class Aprendiz
+    {
+        public string TipoIdentificacion { get; set; }
+        public string NumeroIdentificacion { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Genero { get; set; }
+        public string IDficha { get; set; }
+        public string Estado { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Direccion { get; set; }
+        public string RH { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string TipoDePoblacion { get; set; }
+        public string DepartamentoDeResidencia { get; set; }
+        public string MunicipioDeResidencia { get; set; }
+        public string DepartamentoDeNacimiento { get; set; }
+        public string MunicipioDeNacimiento { get; set; }
+        public string Estrato { get; set; }
+        public string EPS { get; set; }
+        public string TipoDeRegimen { get; set; }
+        public DateTime FechaDeIngreso { get; set; }
+        public string Discapacidad { get; set; }
+        public string LibretaMilitar
+        { get; set; }
+    }
+}
---
+++ Atributos/Atributos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Atributos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aprendiz apSena = new Aprendiz();
+            apSena.Nombres = "cristian camilo";
+            apSena.Apellidos = "jimenez torres";
+            apSena.DepartamentoDeResidencia = "pitalito";
+            saludo(apSena);
+        }
+        private static void saludo(Aprendiz apSena)
+        {
+            Console.WriteLine("Bienvenido Sr.(a)" + apSena.Nombres +" "+ apSena.Apellidos + ", usted vive en: "+ apSena.DepartamentoDeResidencia);
+            Console.ReadLine();
+        }
+    }
+}
